66619273181b453dec627a26a6807f061bec9a14
madekot <mgolubcov@gmail.com>
2018-03-27 17:19:03 +0300

    Исправил localStorage в IE 11 + подравнял JS

--- js/scripts.js
+++ js/scripts.js
@@ -1,64 +1,63 @@
+var link = document.querySelector(".reservation__button-search");
 
-  var link = document.querySelector(".reservation__button-search");
+var formPopup = document.querySelector(".modal-form");
 
-  var formPopup = document.querySelector(".modal-form");
+var arrivalData = document.querySelector("[name=arrival-data]");
+var epartureDate = document.querySelector("[name=departure-date]");
 
-  var arrivalData = document.querySelector("[name=arrival-data]");
-  var epartureDate = document.querySelector("[name=departure-date]");
+var adult = document.querySelector("[id=adult]");
+var children = document.querySelector("[id=children]");
 
-  var adult = document.querySelector("[id=adult]");
-  var children = document.querySelector("[id=children]");
+var isStorageSupport = true;
+var storage = "";
 
-  var storageAdult = localStorage.getItem("adult");
-  var storageChildren = localStorage.getItem("children");
+try {
+  storage = localStorage.getItem("adult");
+  storage = localStorage.getItem("children");
+} catch (err) {
+  isStorageSupport = false;
+}
 
-  var isStorageSupport = true;
-  var storage = "";
-
-  try {
-    storage = localStorage.getItem("adult");
-    storage = localStorage.getItem("children");
-  } catch (err) {
-    isStorageSupport = false;
-  }
-
-  document.addEventListener("DOMContentLoaded", function (evt) {
-    formPopup.classList.add("modal-form__hidden");
+document.addEventListener("DOMContentLoaded", function(evt) {
+  formPopup.classList.add("modal-form__hidden");
 
 });
 
-  link.addEventListener("click", function (evt) {
+link.addEventListener("click", function(evt) {
   evt.preventDefault();
   formPopup.classList.remove("modal-form__error");
   formPopup.classList.toggle("modal-form__show");
-    if (storage) {
-      adult.value = storageAdult;
-      children.value = storageChildren;
-      setTimeout("arrivalData.focus()", 700);
-    } else {
-      setTimeout("arrivalData.focus()", 700);
-    }
+  if (isStorageSupport) {
+    adult.value = storageAdult;
+    children.value = storageChildren;
+    setTimeout("arrivalData.focus()", 900);
+  } else {
+    setTimeout("arrivalData.focus()", 900);
+  }
 });
 
-  formPopup.addEventListener("submit", function (evt) {
-    if (!arrivalData.value || !epartureDate.value || !adult.value || !children.value) {
+var storageAdult = localStorage.getItem("adult");
+var storageChildren = localStorage.getItem("children");
 
-      evt.preventDefault();
-      formPopup.classList.add("modal-form__error");
-    } else {
-        if (isStorageSupport) {
+formPopup.addEventListener("submit", function(evt) {
+  if (!arrivalData.value || !epartureDate.value || !adult.value || !children.value) {
+
+    evt.preventDefault();
+    formPopup.classList.add("modal-form__error");
+  } else {
+    if (storage) {
       localStorage.setItem("adult", adult.value);
       localStorage.setItem("children", children.value);
-      }
     }
+  }
 });
 
-  window.addEventListener("keydown", function (evt) {
-    if (evt.keyCode === 27) {
-      evt.preventDefault();
+window.addEventListener("keydown", function(evt) {
+  if (evt.keyCode === 27) {
+    evt.preventDefault();
 
-      if (formPopup.classList.contains("modal-form__show")) {
+    if (formPopup.classList.contains("modal-form__show")) {
       formPopup.classList.remove("modal-form__show");
-      }
     }
+  }
 });
